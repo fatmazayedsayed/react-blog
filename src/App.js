0e9d11f4431748fe1e14d6f2d0c415b0f9965bce
fatma zayed <fatmazayed@microtec.com.eg>
2022-10-15 22:47:47 +0200

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticleList from './pages/ArticleList';
+import Home from './pages/Home';
 
+import {Route, BrowserRouter,Routes } from'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <BrowserRouter> 
+     <Routes>
+<Route path="/" element= { <Home/>} />
+<Route path="/about" element= { <About/>} />
+<Route path="/articles" element= { <ArticleList/>} />
+<Route path="/articles/:{articleId}" element= { <ArticleList/>} />
+  
+      
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
