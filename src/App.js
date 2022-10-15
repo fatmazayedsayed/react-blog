6b1b8e8c1897729b1558a7ef3c29218730bdfc18
fatma zayed <fatmazayed@microtec.com.eg>
2022-10-15 23:07:41 +0200

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
- import './App.css';
+import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Article from './pages/Article';
 import ArticleList from './pages/ArticleList';
 import Home from './pages/Home';
+import NavBar from './NavBar';
 
-import {Route, BrowserRouter,Routes } from'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-     
-     <BrowserRouter> 
-     <Routes>
-<Route path="/" element= { <Home/>} />
-<Route path="/about" element= { <About/>} />
-<Route path="/articles" element= { <ArticleList/>} />
-<Route path="/articles/:{articleId}" element= { <ArticleList/>} />
-  
-      
+
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:{articleId}" element={<ArticleList />} />
+
+
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
